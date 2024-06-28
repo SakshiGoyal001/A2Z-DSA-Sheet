9419f4ee2d7804c91726dc39ebe70e2dc699f19d
SakshiGoyal001 <100338507+SakshiGoyal001@users.noreply.github.com>
2024-06-28 15:20:09 +0530

    Update Linked List Deletion.cpp

--- Linked List/1. 1D Linked List/Linked List Deletion.cpp
+++ Linked List/1. 1D Linked List/Linked List Deletion.cpp
@@ -1,3 +1,6 @@
+//Here we were given the node to be deleted and not the head
+//https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+
 class Solution {
 public:
     void deleteNode(ListNode* node) {
